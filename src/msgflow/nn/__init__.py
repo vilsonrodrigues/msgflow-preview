08da15c4b8aa3a62717066337932bb3029627884
Vilson Rodrigues <vilsonrodrigues07@hotmail.com>
2025-03-13 00:15:30 -0300

    fix: remove 'parallel' from nn init

--- src/msgflow/nn/__init__.py
+++ src/msgflow/nn/__init__.py
@@ -6,6 +6,5 @@ from msgflow.nn.modules import *  # usort: skip # noqa: F403
 from msgflow.nn import (
     functional as functional,
     modules as modules,
-    parallel as parallel,
     parameter as parameter,
 )        
