43e0e38c65c53371b9c6a72d76a400db44d0e977
Vilson Rodrigues <vilsonrodrigues07@hotmail.com>
2025-03-20 00:50:09 -0300

    add: response_cache decorator

---
+++ src/msgflow/cache.py
@@ -0,0 +1,49 @@
+from functools import wraps
+from typing import Optional
+from msgflow.data.databases.base import BaseDB
+from msgflow.message import Message
+from msgflow.models.types import TextEmbedderModel
+
+def response_cache(
+    db: BaseDB, 
+    task_inputs: Optional[str] = None, 
+    response_mode: Optional[str] = None,
+    model: Optional[TextEmbedderModel] = None,
+):
+    def decorator(func):        
+        @wraps(func)
+        def wrapper(msg):
+            if isinstance(msg, Message):
+                if task_inputs is None:
+                    raise ValueError(
+                        "`task_inputs` is required when input "
+                        "is a Message instance"
+                    )
+                key = msg.get(task_inputs)
+            else:
+                key = msg
+
+            if model:
+                key = model(key)            
+
+            cached_response = db(key)
+            
+            if cached_response is not None:
+                if isinstance(msg, Message):
+                    if response_mode is None:
+                        raise ValueError(
+                            "`response_mode` is required when "
+                            "input is a Message instance"
+                        )
+                    msg.set(response_mode, cached_response)
+                    return msg
+                return cached_response
+            
+            response = func(msg)
+            
+            if response is not None:
+                db.add(key, response)
+
+            return response
+        return wrapper
+    return decorator
