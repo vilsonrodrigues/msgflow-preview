c2b2aac2c5abb070718181c6407ce19cd8f96921
Vilson Rodrigues <vilsonrodrigues07@hotmail.com>
2025-04-08 23:58:27 -0300

    refactor: database

--- src/msgflow/data/databases/types.py
+++ src/msgflow/data/databases/types.py
@@ -1,26 +1,20 @@
-class VectorDB:
-    db_type = "vector_db"
-
-
-class RelationalDB:
-    db_type = "relational_db"
-
-
-class NoSQLDB:
-    db_type = "nosql_db"
-
+class GeoDB:
+    db_type = "geo_db"
 
 class GraphDB:
     db_type = "graph_db"
 
-
-class GeoDB:
-    db_type = "geo_db"
-
-
 class KVDB:
     db_type = "kv_db"
 
+class NoSQLDB:
+    db_type = "nosql_db"
+
+class RelationalDB:
+    db_type = "relational_db"
 
 class TimeSeriesDB:
     db_type = "time_series_db"
+
+class VectorDB:
+    db_type = "vector_db"
