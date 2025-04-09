11aacf1d874f74d91a21950974e7638ca644186c
Vilson Rodrigues <vilsonrodrigues07@hotmail.com>
2025-04-09 00:15:05 -0300

    fix: docstring

--- src/msgflow/data/databases/database.py
+++ src/msgflow/data/databases/database.py
@@ -102,7 +102,7 @@ class DataBase:
         
         Args:
             provider: The db provider (e.g., "faiss", "cachetools")
-            model_type: The type of model (e.g., "vector", "kv")
+            db_type: The type of model (e.g., "vector", "kv")
             params: Dictionary containing the serialized db parameters
             
         Returns:
