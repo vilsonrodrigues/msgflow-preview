eebdc910dfc2492ca7bd6d1c1d41ca6bd2c971c6
Vilson Rodrigues <vilsonrodrigues07@hotmail.com>
2025-05-07 22:42:44 -0300

    add: __all__ in reasoning

--- src/msgflow/generation/reasoning/__init__.py
+++ src/msgflow/generation/reasoning/__init__.py
@@ -2,3 +2,5 @@ from .cot import ChainOfThoughts
 from .react import ReAct
 from .self_consistency import SelfConsistency
 from .tot import TreeOfThoughts
+
+__all__ = [ChainOfThoughts, ReAct, SelfConsistency, TreeOfThoughts]
