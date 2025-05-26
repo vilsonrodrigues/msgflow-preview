d92a24b3662d62e92d2f1cad3d4015dcba4c9212
Vilson Rodrigues <vilsonrodrigues07@hotmail.com>
2025-05-25 22:23:04 -0300

    remove buffer from __init__

--- src/msgflow/nn/__init__.py
+++ src/msgflow/nn/__init__.py
@@ -1,7 +1,4 @@
-from msgflow.nn.parameter import (  # usort: skip
-    Buffer as Buffer,
-    Parameter as Parameter,
-)    
+from msgflow.nn.parameter import Parameter as Parameter # usort: skip
 from msgflow.nn.modules import *  # usort: skip # noqa: F403
 from msgflow.nn import (
     functional as functional,
