ff50d040dad14824395a7e6383c83e36503d9394
Vilson Rodrigues <vilsonrodrigues07@hotmail.com>
2025-05-28 14:17:04 -0300

    refactor: import configure pool from _private now

--- src/msgflow/__init__.py
+++ src/msgflow/__init__.py
@@ -1,3 +1,4 @@
+from ._private.pool import configure_async_pool
 from .cache import response_cache
 from .data.databases.database import DataBase
 from .data.retrievers.retriever import Retriever
@@ -6,7 +7,6 @@ from .generation.signature import InputField, OutputField, Signature
 from .message import Message
 from .models.gateway import ModelGateway
 from .models.model import Model
-from .nn.functional import configure_async_pool
 from .utils.chat import ChatML
 from .utils.inspect import get_fn_name
 from .utils.msgspec import load, save
