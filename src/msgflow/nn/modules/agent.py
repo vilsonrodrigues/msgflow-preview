a686293f515ca0363cae83600287d1d2bd41ce55
Vilson Rodrigues <vilsonrodrigues07@hotmail.com>
2025-06-10 10:08:11 -0300

    refactor xml to dict

--- src/msgflow/nn/modules/agent.py
+++ src/msgflow/nn/modules/agent.py
@@ -904,9 +904,8 @@ class Agent(Module):
         if isinstance(xml_to_dict, bool):
             if xml_to_dict:
                 json_schema = None
-                generation_schema = self.generation_schema
-                if generation_schema:
-                    schema = msgspec.json.schema(generation_schema)                  
+                if self.generation_schema:
+                    schema = msgspec.json.schema(self.generation_schema)                  
                     json_schema = adapt_struct_schema_to_json_schema(schema)                    
                 template_inputs = {
                     "instructions": self.instructions,
