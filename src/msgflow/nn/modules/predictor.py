649626c34c8a50d6f2e57a1342a2d60042068f02
Vilson Rodrigues <vilsonrodrigues07@hotmail.com>
2025-07-01 00:43:34 -0300

    refactor

--- src/msgflow/nn/modules/predictor.py
+++ src/msgflow/nn/modules/predictor.py
@@ -1,4 +1,6 @@
 from typing import Any, Dict, Optional, Union
+
+from msgflow.dotdict import dotdict
 from msgflow.message import Message
 from msgflow.models.base import BaseModel
 from msgflow.models.gateway import ModelGateway
@@ -6,8 +8,9 @@ from msgflow.nn.modules.module import Module
 
 
 class Predictor(Module):
-    """Predictor is a generic Module type that uses Classifier, Regressors, Detectors and Segmenters
-    to generate insights above data.
+    """
+    Predictor is a generic Module type that uses Classifier, Regressors, 
+    Detectors and Segmenters to generate insights above data.
 
     Args:
         name: Predictor name in snake case format.
@@ -27,25 +30,22 @@ class Predictor(Module):
         name: str,
         model: Union[BaseModel, ModelGateway],
         *,
-        task_inputs: Optional[str] = None,
-        task_multimodal_inputs: Optional[Dict[str, str]] = None,        
+        task_inputs: Optional[str] = None,      
         response_mode: Optional[str] = "plain_response",
         response_template: Optional[str] = None,
         execution_kwargs: Optional[Dict[str, Any]] = None,
     ):
         super().__init__()
-        self.set_name(name)    
-        self._set_model(model)    
+        self.set_name(name)
+        self._set_model(model)
         self._set_execution_kwargs(execution_kwargs)
         self._set_response_mode(response_mode)
-        self._set_response_template(response_template)        
-        self._set_task_inputs(task_inputs)
-        self._set_task_multimodal_inputs(task_multimodal_inputs)        
+        self._set_response_template(response_template)
+        self._set_task_inputs(task_inputs)     
 
-    def forward(self, message: Union[Any, Message]):
-        model_preference = self.get_model_preference(message)
-        data = self._prepare_task(message)
-        model_response = self._execute_model(data, model_preference)
+    def forward(self, message: Union[Any, Message], **kwargs) -> Any:
+        inputs = self._prepare_task(message, **kwargs)
+        model_response = self._execute_model(**inputs)
         response = self._process_model_response(model_response, message)
         return response
 
@@ -71,44 +71,26 @@ class Predictor(Module):
                 f"Unsupported model response type `{model_response.response_type}`"
             )
 
-    def _prepare_task(self, message):
+    def _prepare_task(
+        self, message: Union[Any, Message], **kwargs
+    ) -> Dict[str, Any]:
+        inputs = dotdict()
+
         if isinstance(message, Message):
-            data = self._process_message_task(message)
+            data = self._extract_message_values(self.task_inputs, message)
         else:
-            data = message                
-        return data
+            data = message
 
-    def _process_message_task(self, message: Message):
-        if self.task_inputs:
-            content = self._process_task_inputs(message)
-        elif self.task_multimodal_inputs:
-            content = self._process_task_multimodal_inputs(message)
-        else:
-            raise AttributeError(
-                "A message object was passed but neither `task_inputs` "
-                "nor `multimodal_task_inputs` were defined"
-            )            
-        return content
+        inputs.data = data
 
-    def _process_task_inputs(self, message: Message):
-        content = self._get_content_from_message(self.task_inputs, message)
-        return content
+        model_preference = kwargs.pop("model_preference", None)
+        if model_preference is None and isinstance(message, Message):
+            model_preference = self.get_model_preference_from_message(message)
 
-    def _process_task_multimodal_inputs(self, message: Message):
-        content = None
+        if model_preference:
+            inputs.model_preference = model_preference
 
-        audio_path = self.task_multimodal_inputs.get("audio", None)
-        image_path = self.task_multimodal_inputs.get("image", None)
-        file_path = self.task_multimodal_inputs.get("file", None)
-
-        if audio_path:
-            content = self._get_content_from_message(audio_path, message)
-        elif image_path:
-            content = self._get_content_from_message(image_path, message)
-        elif file_path:
-            content = self._get_content_from_message(file_path, message)
-
-        return content
+        return inputs
 
     def _set_model(self, model: Union[BaseModel, ModelGateway]):
         if isinstance(model, (BaseModel, ModelGateway)):
