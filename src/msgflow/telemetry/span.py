c23e114e4aea1d182a5f81a1801ddcb867d83b5c
Vilson Rodrigues <vilsonrodrigues07@hotmail.com>
2025-07-03 15:48:06 -0300

    refactor: adapt trace tool library

--- src/msgflow/telemetry/span.py
+++ src/msgflow/telemetry/span.py
@@ -92,16 +92,12 @@ def trace(
     return decorator                         
 
 def trace_tool_library_call(forward):
-    def wrapper(self, tool_callings):
+    def wrapper(self, tool_callings, *args):
         with self._spans.tool_usage(tool_callings) as span:
-            tool_responses = forward(self, tool_callings)
+            tool_execution_result = forward(self, tool_callings)
             if envs.telemetry_capture_tool_call_responses:
-                responses = []
-                for id, response in tool_responses.items():
-                    responses.append({"id": id, "response": response})
-                encoded_responses = msgspec.json.encode(responses)
-                span.set_attribute("msgflow.nn.tool.responses", encoded_responses)                
-            return tool_responses
+                span.set_attribute("msgflow.nn.tool.responses", tool_execution_result.to_json())
+            return tool_execution_result
     return wrapper
 
 def trace_agent_prepare_model_execution(_prepare_model_execution):
