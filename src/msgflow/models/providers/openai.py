1bbeaba168a5f25e439a3ca11f9036abf3953def
Vilson Rodrigues <vilsonrodrigues07@hotmail.com>
2025-07-07 12:07:03 -0300

    add: usage metadata to embed

--- src/msgflow/models/providers/openai.py
+++ src/msgflow/models/providers/openai.py
@@ -827,7 +827,9 @@ class OpenAITextEmbedder(_BaseOpenAI, TextEmbedderModel):
         response.set_response_type("text_embedding")
         model_output = self._execute_model(**kwargs)
         embedding = model_output.data[0].embedding
+        metadata = dotdict(model_output.usage)
         response.add(embedding)
+        response.set_metadata(metadata)
         return response
 
     @model_retry
