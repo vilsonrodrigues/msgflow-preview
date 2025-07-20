456775c711468dff7d095a29f763e3d34997118f
Vilson Rodrigues <vilsonrodrigues07@hotmail.com>
2025-07-20 14:56:29 -0300

    lint

--- src/msgflow/utils/hooks.py
+++ src/msgflow/utils/hooks.py
@@ -7,20 +7,20 @@ __all__ = ["RemovableHandle"]
 
 
 class RemovableHandle:
-    r"""
-    A handle which provides the capability to remove a hook.
-
-    Args:
-        hooks_dict (dict): A dictionary of hooks, indexed by hook ``id``.
-        extra_dict (Union[dict, List[dict]]): An additional dictionary or list of
-            dictionaries whose keys will be deleted when the same keys are
-            removed from ``hooks_dict``.
-    """
+    """A handle which provides the capability to remove a hook."""
 
     id: int
     next_id: int = 0
 
     def __init__(self, hooks_dict: Any, *, extra_dict: Any = None) -> None:
+        """
+        Args:
+            hooks_dict: 
+                A dictionary of hooks, indexed by hook `id`.
+            extra_dict:
+                An additional dictionary or list of dictionaries whose keys 
+                will be deleted when the same keys are removed from `hooks_dict`.
+        """
         self.hooks_dict_ref = weakref.ref(hooks_dict)
         self.id = RemovableHandle.next_id
         RemovableHandle.next_id += 1
